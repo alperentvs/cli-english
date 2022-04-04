65f9c0d90fc0a7f28323cb7d14c1c2d35f6c5259
alisezisli <ali@alisezisli.com.tr>
2022-04-04 03:46:06 +0300

    Added random word picker
    Added random word picker
    Added file name to variable

--- cli-english.sh
+++ cli-english.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
-# Get word list file:
-if [[ ! -f /tmp/clienglishwordlist ]]
+wlfile="/tmp/clienglishwordlist"
+
+#Get word list file:
+if [[ ! -f ${wlfile} ]]
 then
-	curl -o /tmp/clienglishwordlist https://gnuadm.in/eng.md 2> /dev/null
+	curl -o ${wlfile} https://gnuadm.in/eng.md 2> /dev/null
 fi
 
-# Get word count:
+#Get word count:
 wordcount=$(cat /tmp/clienglishwordlist | wc -l)
 
-# Get random wordID:
+#Get random wordID:
 wordid=$((1 + $RANDOM % ${wordcount}))
+
+#Get random word:
+randomword=$(cat ${wlfile} | sed -n "${wordid}p")
